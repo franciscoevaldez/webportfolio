e5e71a8dadbac905dc9d6943d3a1b783d7b53326
franciscoevaldez <franciscoevaldez@gmail.com>
2015-05-08 15:57:32 -0300

    work listo
    Trabajo listo

--- script/site.js
+++ script/site.js
@@ -1,19 +1,101 @@
 $(document).ready(function() {
 
-	$(window).scroll(function() {
+	if (!$('body').hasClass("work")) {
 
-		if ($(this).scrollTop() > 200){  
+		$(window).scroll(function() {
 
-			$('header').addClass("stacked");
-			$('.firstContent').addClass("padded");
+			if ($(this).scrollTop() > 200){  
 
-		} else {
+				$('header').addClass("stacked");
+				$('.firstContent').addClass("padded");
 
-			$('header').removeClass("stacked");
-			$('.firstContent').removeClass("padded");
+			} else {
 
+				$('header').removeClass("stacked");
+				$('.firstContent').removeClass("padded");
+
+			}
+
+		});
+
+	};
+
+
+	if ($('body').hasClass("work")) {
+
+		function disableAutoHide(){
+			$('body').addClass("fxDontHide");
 		}
 
-	});
+		function enableAutoHide(){
+			$('body').removeClass("fxDontHide");
+		}		
+
+		$(window).scroll(function() {
+
+			if ($(window).scrollTop() > 200 && !$('body').hasClass("fxDontHide")){  
+
+				enableAutoHide();
+
+				$('a#viewdetails').removeClass("noShow");
+				$('a#closedetails').addClass("noShow");
+
+				$('#descriptionrow').slideUp();
+
+			}
+
+			if ($(window).scrollTop() < 0){  
+
+				enableAutoHide();
+
+				$('a#closedetails').removeClass("noShow");
+				$('a#viewdetails').addClass("noShow");
+
+				$('article#work').addClass("padded");
+				$('article#work').removeClass("paddedShort");
+
+				$('#descriptionrow').slideDown();
+
+			}
+
+		});
+
+		$('a#closedetails').click(function() {
+
+			enableAutoHide();
+
+			$('a#viewdetails').removeClass("noShow");
+			$('a#closedetails').addClass("noShow");
+
+			if ($(window).scrollTop() == 0){
+
+				$('article#work').addClass("paddedShort");
+				$('article#work').removeClass("padded");
+
+			}
+
+			$('#descriptionrow').slideUp();
+
+		});
+
+		$('a#viewdetails').click(function() {
+
+			disableAutoHide();
+
+			$('a#closedetails').removeClass("noShow");
+			$('a#viewdetails').addClass("noShow");
+
+			if ($(window).scrollTop() == 0){
+
+				$('article#work').addClass("padded");
+				$('article#work').removeClass("paddedShort");
+
+			}
+
+			$('#descriptionrow').slideDown();
+
+		});
+
+	};
 
 });
